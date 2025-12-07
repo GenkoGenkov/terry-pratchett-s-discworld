d99303f39fded519ac443e438fa9e58e31de0fdc
GenkoGenkov <werto.haho@gmail.com>
2025-12-07 21:51:06 +0200

    Adding logout and refactoring the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,17 +9,38 @@ import Details from "./components/details/Details"
 import BookCreate from "./components/book-create/BookCreate"
 import Register from "./components/register/Register"
 import Login from "./components/login/Login"
+import Logout from "./components/logout/Logout"
 
 
 
 function App() {
-    const[user, setUser] = useState(null);
+    const [registerdUsers, setRegisteredUsers] = useState([]);
+    const [user, setUser] = useState(null);
 
-    const authHandler = (email) => {
-      setUser({
-        email,
-      });
-    }
+    const registerHandler = (email, password) => {
+        if (registerdUsers.some(user => user.email === email)) {
+            throw new Error('Email is not avaliable!');
+        }
+
+        const newUser = { email, password };
+
+        setRegisteredUsers(state => [...state, newUser]);
+
+        setUser(newUser);
+    };
+
+    const loginHandler = (email, password) => {
+        const user = registerdUsers.find(u => u.email === email && u.password === password);
+        if (!user) {
+            throw new Error('Invalid email or password')
+        }
+
+        setUser(user);
+    };
+
+    const logoutHandler = () => {
+        setUser(null);
+    };
 
     return (
     <>
@@ -30,8 +51,9 @@ function App() {
             <Route path="/books" element={<Catalog/>} />
             <Route path="/books/:bookId/details" element={<Details />} />
             <Route path="/books/create" element={<BookCreate />} />
-            <Route path="/register" element={<Register onRegister={authHandler} />} />
-            <Route path="/login" element={<Login onLogin={authHandler} />} />
+            <Route path="/register" element={<Register onRegister={registerHandler} />} />
+            <Route path="/login" element={<Login onLogin={loginHandler} />} />
+            <Route path="/logout" element={<Logout onLogout={logoutHandler} />} />
         </Routes>
 
      <Footer/>
